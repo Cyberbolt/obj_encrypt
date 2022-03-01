9cad6bd3ce60f4bef3ab667e4ee1b45f585676ba
Cyberbolt <735245473@qq.com>
2022-03-01 22:03:55 +0800

    Change the internal algorithm to improve performance.

--- obj_encrypt/__init__.py
+++ obj_encrypt/__init__.py
@@ -1,6 +1,43 @@
+import sys
+import base64
 import pickle
 
+from Crypto.Cipher import AES
+from Crypto import Random
 from AesEverywhere import aes256
+
+
+import hashlib
+
+
+BLOCK_SIZE = 16
+KEY_LEN = 32
+IV_LEN = 16
+
+
+class Signature:
+    '''
+        This class is based on the SHA-256 algorithm for signing and checking data integrity.
+    '''
+
+    def __init__(self, salt: bytes, iterations: int=1):
+        '''
+            salt -- SHA-256 salt\n
+            iterations -- The number of iterations of the SHA-256 algorithm.
+        '''
+        if type(salt) != type(b'1'):
+            raise RuntimeError('Salt must be bytes.')
+        self._salt = salt
+        self._iterations = iterations
+
+    def encrypt(self, content: bytes):
+        '''
+            SHA-256 encryption
+        '''
+        if type(content) != type(b'1'):
+            raise RuntimeError('Content must be bytes.')
+        dk = hashlib.pbkdf2_hmac('sha256', content, self._salt, self._iterations)
+        return dk.hex()
 
 
 class Secret:
@@ -9,14 +46,20 @@ class Secret:
         # A 256 bit (32 byte) key
         if type(key) != type(''):
             raise RuntimeError('The key must be a string.')
-        if len(key) > 32:
+        if len(key) > KEY_LEN:
             raise RuntimeError('The key cannot contain more than 32 characters.')
+        
         self.__key = key
         # Less than 32 characters complement 0.
-        for i in range(32 - len(key)):
+        for i in range(KEY_LEN - len(key)):
             self.__key += '0'
+        
+        # Used for SHA 256 signatures.
+        key_bin = self.__key.encode()
+        self.signature = Signature(salt=key_bin)
+        self.__iv = self.signature.encrypt(key_bin)[:IV_LEN]
 
-    def encrypt(self, obj) -> bytes:
+    def encrypt_old(self, obj) -> bytes:
         '''
             Encrypt Python objects using AES.
                 obj -- Most Python objects.
@@ -26,7 +69,7 @@ class Secret:
         ciphertext = aes256.encrypt(obj_bin_str, self.__key)
         return ciphertext
 
-    def decrypt(self, ciphertext: bytes):
+    def decrypt_old(self, ciphertext: bytes):
         '''
             AES decryption.
                 ciphertext -- The ciphertext is encrypted by encrypt method.
@@ -41,3 +84,56 @@ class Secret:
         # Convert the binary to a Python object.
         obj = pickle.loads(obj_bin)
         return obj
+
+    def encrypt(self, obj) -> bytes:
+        aes = AES.new(self.__key, AES.MODE_CBC, self.__iv)
+        
+        # Convert Python objects to binary.
+        obj_bin = pickle.dumps(obj)
+        # Get binary string.
+        obj_str = obj_bin.hex()
+        
+        # The padding string size is a multiple of 16B.
+        for i in range(16 - len(obj_str) % BLOCK_SIZE):
+            obj_str += ' '
+
+        ciphertext = aes.encrypt(obj_str)
+        return ciphertext
+    
+    def decrypt(self, ciphertext: bytes):
+        aes = AES.new(self.__key, AES.MODE_CBC, self.__iv)
+        
+        obj_str = aes.decrypt(ciphertext).decode()
+        
+        # Unpack the string padding.
+        obj_str = obj_str.rstrip(' ')
+        
+        # Convert the string to its original binary.
+        obj_bin = bytes.fromhex(obj_str)
+        # Convert binary to Python object.
+        obj = pickle.loads(obj_bin) 
+        return obj
+    
+
+def pkcs7_padding(s):
+    """
+    Padding to blocksize according to PKCS #7
+    calculates the number of missing chars to BLOCK_SIZE and pads with
+    ord(number of missing chars)
+    @see: http://www.di-mgt.com.au/cryptopad.html
+    @param s: string Text to pad
+    @type s: string
+    @rtype: string
+    """
+    s_len = s.encode('utf-8')
+    s = s + (BLOCK_SIZE - s_len % BLOCK_SIZE) * chr(BLOCK_SIZE - s_len % BLOCK_SIZE)
+    return bytes(s, 'utf-8')
+
+def pkcs7_trimming(s):
+    """
+    Trimming according to PKCS #7
+    @param s: string Text to unpad
+    @type s: string
+    @rtype: string
+    """
+    return s[0:-s[-1]]
--- test.py
+++ test.py
@@ -7,11 +7,12 @@ def main():
     # Initialize the secret instance, the key is an AES-256 key, and the maximum size cannot exceed 32 strings.
     secret = Secret(key='0123456789')
     # build the data dictionary
-    data = {
-        'author': 'Cyberbolt',
-        'personal_website': 'https://www.cyberlight.xyz/',
-        'time': '2022-02-10'
-    }
+    # data = {
+    #     'author': 'Cyberbolt',
+    #     'personal_website': 'https://www.cyberlight.xyz/',
+    #     'time': '2022-02-10'
+    # }
+    data =['test']
     
     start1 = time.time()    
     ciphertext = secret.encrypt(data) # Convert the object to binary ciphertext and get the ciphertext.
